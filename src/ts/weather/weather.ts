9029a155a9e59fe1f0607a0c996f76f35864156a
ehnvik <gustav.ehnvik@gmail.com>
2022-11-18 20:45:20 +0100

    Added weather update for the user in footer

--- src/ts/weather/weather.ts
+++ src/ts/weather/weather.ts
@@ -4,35 +4,64 @@ import { IWeatherData } from "../models/IWeatherData";
 import { Weather } from "../models/Weather";
 import { WeatherDetails } from "../models/WeatherDetails";
 
-let lat: number = 59.358268;
-let lon: number = 17.905318;
+if (navigator.geolocation) {
+  navigator.geolocation.getCurrentPosition(success, error);
+}
+
+function success(position: GeolocationPosition) {
+  let lat: number = position.coords.latitude;
+  let lon: number = position.coords.longitude;
+  getWeatherApi(lat, lon);
+}
+
+function error(e: GeolocationPositionError) {
+  console.log("No Position");
+  let lat: number = 59.329296;
+  let lon: number = 18.069643;
+  getWeatherApi(lat, lon);
+}
 
 let apiKey = "020edece24a6c3fb1efcc31fd47010d6";
+function getWeatherApi(lat: number, lon: number) {
+  axios
+    .get<IWeatherData>(
+      `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&lang=sv&appid=${apiKey}&units=metric`
+    )
+    .then((response) => {
+      let myWeather: WeatherDetails[] = response.data.weather.map(
+        (weatherData: IWeather) => {
+          return new WeatherDetails(weatherData.main, weatherData.description);
+        }
+      );
 
-axios
-  .get<IWeatherData>(
-    `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}&units=metric`
-  )
-  .then((response) => {
-    // console.log(response.data);
-    let myWeather: WeatherDetails[] = response.data.weather.map(
-      (weatherData: IWeather) => {
-        return new WeatherDetails(weatherData.main, weatherData.description);
-      }
-    );
-
-    let weather = new Weather(
-      response.data.main.temp,
-      response.data.name,
-      myWeather
-    );
-
-    console.log(weather);
-    printWeatherData(weather);
-  });
+      let weather = new Weather(
+        response.data.main.temp,
+        response.data.name,
+        myWeather
+      );
+      printWeatherData(weather);
+    });
+}
 
 function printWeatherData(newWeather: Weather) {
+  let weatherLocation: HTMLLinkElement = document.getElementById(
+    "weather-location"
+  ) as HTMLLinkElement;
+
+  let weatherTemperature: HTMLLinkElement = document.getElementById(
+    "weather-temperature"
+  ) as HTMLLinkElement;
+
+  let weatherDescription = "";
+
   newWeather.weather.forEach((weather: WeatherDetails) => {
-    console.log(weather.main);
+    weatherDescription = weather.description;
   });
+
+  let wholeNumberTemperature: number = Math.round(newWeather.temperature);
+  let temperatureText = wholeNumberTemperature.toString();
+
+  weatherLocation.innerHTML = newWeather.locationName;
+  weatherTemperature.innerHTML =
+    temperatureText + " &deg;C" + " " + weatherDescription;
 }
